f9c02936e1c0810aed8cbc281583df8ba02e7232
Ryan Norris <r.norris@samsung.com>
2023-04-08 17:03:01 -0400

    lemonadep: Add initial support.

---
+++ Platforms/SM8350Pkg/Devices/lemonadep/Library/PlatformMemoryMapLib/PlatformMemoryMap.c
@@ -0,0 +1,22 @@
+#include <Library/BaseLib.h>
+#include <Library/PlatformMemoryMapLib.h>
+
+static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
+    /* Name               Address     Length      HobOption        ResourceAttribute    ArmAttributes
+                                                          ResourceType          MemoryType */
+
+    /* DDR Regions */
+
+    /* RAM partition regions */
+
+    /* Other memory regions */
+
+    /* Register regions */
+
+    /* Terminator for MMU */
+    {"Terminator", 0, 0, 0, 0, 0, 0, 0}};
+
+ARM_MEMORY_REGION_DESCRIPTOR_EX *GetPlatformMemoryMap()
+{
+    return gDeviceMemoryDescriptorEx;
+}
